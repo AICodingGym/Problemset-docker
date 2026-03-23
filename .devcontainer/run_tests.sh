f3ab7a2f9d5ea11b079989613b780ef28afa78dd
Qiyu Li <qil047@ucsd.edu>
2026-03-23 07:45:24 +0000

    feat: container workflow + devcontainer + template files

---
+++ .devcontainer/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate testbed
+
+# Run tests
+if [ $# -eq 0 ]; then
+  tox --current-env -epy39 -v -- tests/test_markup.py
+else
+  tox --current-env -epy39 -v -- "$@"
+fi
