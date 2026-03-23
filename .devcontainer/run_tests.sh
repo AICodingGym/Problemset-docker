038e2709c364a38ffb7f8de5c0e4b468fda5f7ca
Qiyu Li <qil047@ucsd.edu>
2026-03-23 07:41:04 +0000

    feat: container workflow + devcontainer + template files

---
+++ .devcontainer/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate testbed
+
+# Run tests
+if [ $# -eq 0 ]; then
+  ./tests/runtests.py --verbosity 2 --settings=test_sqlite --parallel 1 tests/staticfiles_tests/test_storage.py
+else
+  ./tests/runtests.py --verbosity 2 --settings=test_sqlite --parallel 1 "$@"
+fi
