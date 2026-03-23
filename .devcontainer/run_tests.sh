8194a919788d29823c059e961b49bf7d2910a1ea
Qiyu Li <qil047@ucsd.edu>
2026-03-23 07:45:06 +0000

    feat: container workflow + devcontainer + template files

---
+++ .devcontainer/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate testbed
+
+# Run tests
+if [ $# -eq 0 ]; then
+  pytest -rA sklearn/ensemble/tests/test_voting.py
+else
+  pytest -rA "$@"
+fi
